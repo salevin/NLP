dede7b73288bdd90f0d3c68df5d4364316063425
sam <samueljlevin@gmail.com>
2015-11-17 09:34:49 -0500

    Python 2.6 workaround

--- Exercise2.py
+++ Exercise2.py
@@ -1,11 +1,10 @@
-from collections import Counter
-
+import CounterWorkaround
 import nltk
 
 tags = nltk.corpus.brown.tagged_words()
 
 if __name__ == '__main__':
-    occurs = Counter([i[0] for i in tags])
+    occurs = CounterWorkaround.Counter([i[0] for i in tags])
     most_common = occurs.most_common(1)[0][0]
     over_two = list(i for i in occurs if occurs.get(i) >= 2)
     print("The most common word is \"%s\"" % most_common)
--- code_cascaded_chunker.py
+++ code_cascaded_chunker.py
@@ -2,16 +2,22 @@
 import nltk
 
 grammar = r"""
-  NP: {<DT|JJ|NN.*>+}          # Chunk sequences of DT, JJ, NN
+  NP: {<DT|JJ|NN.*>+ <VBG>?<NN>?}          # Chunk sequences of DT, JJ, NN
   PP: {<IN><NP>}               # Chunk prepositions followed by NP
   VP: {<VB.*><NP|PP|CLAUSE>+$} # Chunk verbs and their arguments
   CLAUSE: {<NP><VP>}           # Chunk NP, VP
   """
-cp = nltk.RegexpParser(grammar,loop=2)
+cp = nltk.RegexpParser(grammar, loop=2)
 sentence1 = [("Mary", "NN"), ("saw", "VBD"), ("the", "DT"), ("cat", "NN"),
-    ("sit", "VB"), ("on", "IN"), ("the", "DT"), ("mat", "NN")]
+             ("sit", "VB"), ("on", "IN"), ("the", "DT"), ("mat", "NN")]
 
 sentence2 = [("John", "NNP"), ("thinks", "VBZ"), ("Mary", "NN"),
-     ("saw", "VBD"), ("the", "DT"), ("cat", "NN"), ("sit", "VB"),
-     ("on", "IN"), ("the", "DT"), ("mat", "NN")]
+             ("saw", "VBD"), ("the", "DT"), ("cat", "NN"), ("sit", "VB"),
+             ("on", "IN"), ("the", "DT"), ("mat", "NN")]
 
+sentence3 = [("the", "DT"), ("receiving", "VBG"), ("end", "NN")]
+
+sentence4 = [("assistant", "NN"), ("managing", "VBG"), ("editor", "NN")]
+
+if __name__ == '__main__':
+    print (cp.parse(sentence1))
